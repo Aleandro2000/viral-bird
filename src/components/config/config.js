66f88501ed6df2495af889c559bb270a8e542c4c
Alexandru Carmici <66217150+Aleandro2000@users.noreply.github.com>
2021-12-23 19:38:15 +0200

    Update config.js

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -4,14 +4,14 @@ import "firebase/database";
 import "firebase/storage";
 
 const config={
-    apiKey: "AIzaSyBuvJclGWjXcp3Ho6CQW6bwXg-PYlYYykg",
-    authDomain: "softbinator-social-media-bf28f.firebaseapp.com",
-    databaseURL: "https://softbinator-social-media-bf28f-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "softbinator-social-media-bf28f",
-    storageBucket: "softbinator-social-media-bf28f.appspot.com",
-    messagingSenderId: "954112786631",
-    appId: "1:954112786631:web:4f1610933f3c91ad5c7ee2",
-    measurementId: "G-KVYM1ZHBV6"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 function initFirebase() {
